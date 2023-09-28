3f03e0cd96b24d8b1099a60e01033a58a8e1c455
abuobaida <abuobaida@users.noreply.github.com>
2023-09-28 08:12:14 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,24 +1,23 @@
 <?php
+    // Get the raw request data from php://input
+    $get_data = file_get_contents('php://input');
+    // Decode the JSON data
+    $data = json_decode($get_data, true);
+    // Check if the decoding was successful
+    if ($data !== null) {
+        // Print the entire decoded array
+        $decodedData = $data['data'];
+        // print_r($decodedData);
+        $totalPages = counts($decodedData);
+        echo $totalPages;
+    } else {
+        echo “No pages found“;
+    }
 
-// $path = $_POST['file'];
-
-
-$get_data = file_get_contents('php://input');
-file_put_contents("pdf_data.txt",$get_data);
-
-$data = file_get_contents("pdf_data.txt");
-// echo "<pre>";print_r($data);
-
-
-$totalPages = counts($data);
-echo $totalPages;
-
-function counts($data)
-{
-  $pdf = base64_decode($data);
-  // echo $pdf;
-  $number = preg_match_all("/\/Page\W/", $pdf, $dummy);
-  return $number;
-}
-
+    function counts($data)
+    {
+        $pdf = base64_decode($data);
+        $number = preg_match_all("/\/Page\W/", $pdf, $dummy);
+        return $number;
+    }
 ?>
