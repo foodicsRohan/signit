c9034e444081130a775e6484e1a3b3fda80a3c69
foodicsRohan <145523082+foodicsRohan@users.noreply.github.com>
2023-10-13 15:35:22 +0530

    Add files via upload

---
+++ NafithVariable.php
@@ -0,0 +1,9 @@
+<?
+// Nafith BODY  FILE IN THE FOLDER
+ $Authorization = "MnlkZnJjWVBNOVNyWW9lZU5pcWR5ZDNkVDJkdzdmeTVyWFJnMVhXajpPakRiVXFNSEVlcDdQUUVvVnRpTmJhUWpnYnc2MlUxTG5TU2c4cnBkRW1CZlRHQTFGSW9KNTNQY3dKMDVQWTllZW5aMTNHTExXZ2p4emg4eW9TQlo0aVViSTZSenI0WUI5QnR2amltVUF3eDE5M3lNU3RnS3ZFMHlhUjQyOWxldQ==";
+ $secret_key = "rwGoV7s8dBY1J7zZjcJAjWOazEVs0dIsEw7KUHEkiBAwUcTnmQV8pgXkMCrXdIVNQLguTNTwQPW4aj81rM42TIJcY3UC22iOhuTDq1ZxsYa0IggWpu2qjf1LwhexDqrs";
+ $NafithTokenURL = 'https://sandbox.nafith.sa/api/oauth/token/';
+ $CreateSanadURL = 'https://sandbox.nafith.sa/api/sanad-group/';
+
+?>
+
